2d3871e9d10943d34668442e0d1ca98560061835
slavyana <slavyana.c@gmail.com>
2020-04-28 22:48:03 +0100

    Added javascript to home page

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
@@ -7,7 +9,19 @@ def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
 
+def tweet_list_view(request, *args, **kwargs):
+    # REST API VIEW
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content, "likes": random.randint(0, 100)} for x in qs]
+    data = {
+        'isUser': False,
+        'response': tweets_list
+    }
+    return JsonResponse(data)
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    # REST API VIEW
 
     data = {
         'id': tweet_id,
