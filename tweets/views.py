44bb98afe2739abf9b020e3b8637e9b16b25f1d3
slavyana <slavyana.c@gmail.com>
2020-04-29 20:19:22 +0100

    Updated tweet order and reloading of tweets when a tweet is created

--- tweets/views.py
+++ tweets/views.py
@@ -23,7 +23,7 @@ def tweet_create_view(request, *args, **kwargs):
         # do other form related logic
         obj.save()
         if request.is_ajax():
-            return JsonResponse({}, status=201)
+            return JsonResponse(obj.serialize(), status=201)
         if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -34,7 +34,7 @@ def tweet_create_view(request, *args, **kwargs):
 def tweet_list_view(request, *args, **kwargs):
     # REST API VIEW
     qs = Tweet.objects.all()
-    tweets_list = [{'id': x.id, 'content': x.content, "likes": random.randint(0, 100)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         'isUser': False,
         'response': tweets_list
