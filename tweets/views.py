ef8c81d006e654c2f0c0dea5f381a4035d487682
slavyana <slavyana.c@gmail.com>
2020-04-30 12:25:37 +0100

    Added authentication and permission classes to api views.

--- tweets/views.py
+++ tweets/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .forms import TweetForm
 from .models import Tweet
@@ -20,6 +22,8 @@ def home_view(request, *args, **kwargs):
 
 
 @api_view(['POST'])
+#@authentication_classes([SessionAuthentication, MyCustomAuth])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST or None)
     if serializer.is_valid(raise_exception=True):
