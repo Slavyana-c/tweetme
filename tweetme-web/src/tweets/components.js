b515afb591d7db26b3d1c7028a3fdb063ba56f1c
slavyana <slavyana.c@gmail.com>
2020-05-02 17:52:10 +0100

    Render retweets.

--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -92,7 +92,7 @@ export function ActionBtn(props) {
 
     const handleActionBackendEvent = (response, status) => {
         console.log(response, status);
-        if(status === 200) {
+        if (status === 200) {
             setLikes(response.likes)
         }
     };
@@ -106,12 +106,27 @@ export function ActionBtn(props) {
     return <button className={className} onClick={handleClick}>{display}</button>
 }
 
+export function ParentTweet(props) {
+
+    const {tweet} = props
+    return tweet.parent ? <div className={'row'}>
+        <div className={'col-11 mx-auto p-3 border rounded'}>
+            <p className={'mb-0 text-muted small'}>Retweet</p>
+            <Tweet className={' '} tweet={tweet.parent}/>
+        </div>
+    </div> : null
+}
+
 
 export function Tweet(props) {
     const {tweet} = props;
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6';
     return <div className={className}>
-        <p>{tweet.content}</p>
+        <div>
+            <p>{tweet.id} - {tweet.content}</p>
+
+            <ParentTweet tweet={tweet}/>
+        </div>
         <div className={'btn btn-group'}>
             <ActionBtn tweet={tweet} action={{type: 'like', display: "Likes"}}/>
             <ActionBtn tweet={tweet} action={{type: 'unlike', display: "Unlike"}}/>
