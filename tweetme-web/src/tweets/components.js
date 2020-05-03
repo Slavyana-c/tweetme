cd2c13e3eb44f58514b98475c9d5f4a45c7c04ff
slavyana <slavyana.c@gmail.com>
2020-05-03 15:05:20 +0100

    Update file structure

--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -1,5 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {apiTweetList, apiTweetCreate, apiTweetAction} from "./lookup";
+import React, {useState} from "react";
+import {apiTweetCreate} from "./lookup";
+import {TweetsList} from "./list";
+import {TweetCreate} from "./create";
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef();
@@ -11,14 +13,8 @@ export function TweetsComponent(props) {
     const handleBackendUpdate = (response, status) => {
         // backend api response handler
         let tempNewTweets = [...newTweets];
-        if (status === 201) {
-            tempNewTweets.unshift(response);
-            setNewTweets(tempNewTweets);
-
-        } else {
-            console.log(response);
-            alert("An error occurred. Please try again.")
-        }
+        tempNewTweets.unshift(response);
+        setNewTweets(tempNewTweets)
     };
 
     const handleSubmit = (event) => {
@@ -33,142 +29,9 @@ export function TweetsComponent(props) {
 
     return <div className={props.className}>
         {canTweet === true &&
-        <div className={'col-12 mb-3'}>
-            <form onSubmit={handleSubmit}>
-             <textarea ref={textAreaRef} required={true} className={'form-control'} name={'tweet'}>
-
-            </textarea>
-                <button type={'submit'} className={'btn btn-primary my-3'}>Tweet</button>
-
-            </form>
-        </div>}
+        <TweetCreate didTweet={handleBackendUpdate} className={'col-12 mb-3'}/>}
         {/*pass all props from this component*/}
         <TweetsList newTweets={newTweets} {...props}/>
-
-    </div>
-}
-
-export function TweetsList(props) {
-    const [tweetsInit, setTweetsInit] = useState([]);
-    const [tweets, setTweets] = useState([]);
-    const [tweetsDidSet, setTweetsDidSet] = useState(false);
-
-
-    useEffect(() => {
-        const final = [...props.newTweets.concat(tweetsInit)];
-        if (final.length !== tweets.length) {
-            setTweets(final)
-        }
-
-    }, [props.newTweets, tweets, tweetsInit]);
-
-    useEffect(() => {
-        if (tweetsDidSet === false) {
-            const handleTweetListLookup = (response, status) => {
-                if (status === 200) {
-                    setTweetsInit(response);
-                    setTweetsDidSet(true)
-                } else {
-                    console.log(response);
-                    alert("There was an error")
-                }
-            };
-            apiTweetList(props.username, handleTweetListLookup)
-
-        }
-
-    }, [tweetsInit, tweetsDidSet, setTweetsDidSet, props.username]);
-
-    // Update both states to work with create tweet and retweet
-    const handleDidRetweet = (newTweet) => {
-        const updateTweetsInit = [...tweetsInit];
-        updateTweetsInit.unshift(newTweet);
-        setTweetsInit(updateTweetsInit);
-        const updateFinalTweets = [...tweets];
-        updateFinalTweets.unshift(tweets);
-        setTweets(updateFinalTweets)
-
-    };
-
-    return tweets.map((item, index) => {
-        return <Tweet
-            tweet={item}
-            didRetweet={handleDidRetweet}
-            className='my-5 py-5 border bg-white text-dark'
-            key={`${index} - ${item.id}`}/>
-    })
-}
-
-export function ActionBtn(props) {
-    const {tweet, action, didPerformAction} = props;
-    const likes = tweet.likes ? tweet.likes : 0;
-    const className = props.className ? props.className : 'btn btn-primary btn-sm';
-    const actionDisplay = action.display ? action.display : 'Action';
-    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : action.display;
-
-    const handleActionBackendEvent = (response, status) => {
-        console.log(response, status);
-        if ((status === 200 || status === 201) && didPerformAction) {
-            //setLikes(response.likes)
-            didPerformAction(response, status)
-        }
-    };
-
-    const handleClick = (event) => {
-        event.preventDefault();
-        apiTweetAction(tweet.id, action.type, handleActionBackendEvent)
-
-
-    };
-    return <button className={className} onClick={handleClick}>{display}</button>
-}
-
-export function ParentTweet(props) {
-
-    const {tweet} = props;
-    return tweet.parent ? <div className={'row'}>
-        <div className={'col-11 mx-auto p-3 border rounded'}>
-            <p className={'mb-0 text-muted small'}>Retweet</p>
-            <Tweet hideActions className={' '} tweet={tweet.parent}/>
-        </div>
-    </div> : null
-}
-
-
-export function Tweet(props) {
-    const {tweet, didRetweet, hideActions} = props;
-    // writing tweet instead of props.tweet should also work
-    const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null);
-    const className = props.className ? props.className : 'col-10 mx-auto col-md-6';
-
-    const handlePerformAction = (newActionTweet, status) => {
-        if (status === 200) {
-            setActionTweet(newActionTweet)
-
-        } else if (status === 201) {
-            // let the tweet list know
-            if (didRetweet) {
-                didRetweet(newActionTweet)
-
-            }
-        }
-    };
-
-
-    return <div className={className}>
-        <div>
-            <p>{tweet.id} - {tweet.content}</p>
-
-            <ParentTweet tweet={tweet}/>
-        </div>
-        {(actionTweet && hideActions !== true) && <div className={'btn btn-group'}>
-            <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction}
-                       action={{type: 'like', display: "Likes"}}/>
-            <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction}
-                       action={{type: 'unlike', display: "Unlike"}}/>
-            <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction}
-                       action={{type: 'retweet', display: "Retweet"}}/>
-        </div>}
 
     </div>
 }
