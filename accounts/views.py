ecdfe4047a35ae50c1a322cf243cb5138c032393
slavyana <slavyana.c@gmail.com>
2020-05-04 12:53:01 +0100

    Added authentication - register, login, logout

---
+++ accounts/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+# Create your views here.
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    # if you have your own form
+    # form = MyModelForm(request.POST or None )
+
+    if form.is_valid():
+        # username = form.cleaned_data.get('username')
+        # user_ = authenticate(username, password)
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect('/')
+    context = {
+        'form': form,
+        'btn_label': 'Login',
+        'title': 'Login'
+    }
+    return render(request, 'accounts/auth.html', context)
+
+
+def logout_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login')
+
+    context = {
+
+        'form': None,
+        'description': 'Are you sure you want to logout?',
+        'btn_label': 'Click to confirm',
+        'title': 'Logout'
+    }
+
+    return render(request, 'accounts/auth.html', context)
+
+
+def register_view(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+
+    if form.is_valid():
+        user = form.save(commit=True)
+        user.set_password(form.cleaned_data.get('password1'))
+        # send confirmation email here
+        login(request, user)
+        return redirect('/')
+        # print(form.cleaned_data)
+        # username = form.cleaned_data.get('username')
+        # User.objects.create(username=username)
+
+    context = {
+        'form': form,
+        'btn_label': 'Register',
+        'title': 'Register'
+    }
+    return render(request, 'accounts/auth.html', context)
--- tweetme/settings.py
+++ tweetme/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
     # internal
     'tweets',
+    'accounts'
 ]
 
 MIDDLEWARE = [
